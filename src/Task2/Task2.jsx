bc1b39c3a8b31c4817bcc5812da048860481c62f
Eirik Luka <eirik.luka@gmail.com>
2021-03-29 20:33:19 +0200

    Gi en bekreftelse på at skjemaet er godkjent, og at man kan gå videre

--- src/Task2/Task2.jsx
+++ src/Task2/Task2.jsx
@@ -1,64 +1,89 @@
+import { useState } from "react";
 import styles from "./Task2.module.scss";
 
 const Task2 = ({ onSubmit }) => {
+  const [showResults, setShowResults] = useState(false);
   const submitForm = (e) => {
     e.preventDefault();
+    setShowResults(true);
+  };
 
-    console.log(e);
-    // gi beskjed
-    // gå til neste side
+  const advance = (e) => {
+    e.preventDefault();
     onSubmit();
   };
+
   return (
-    <form onSubmit={submitForm}>
-      <h1 className={styles.title}>Send inn dette skjemaet for å gå videre</h1>
-      <div className={styles.container}>
-        <div className={styles.formGrouping}>
-          <label className={styles.inputLabel}>
-            <span>Hva heter du?</span>
-            <input
-              required
-              name="first"
-              aria-describedby="nameDescription"
-              aria-errormessage="Fyll inn fornavnet ditt"
-            />
-            <div id="nameDescription" className={styles.visuallyHidden}>
-              Fyll inn fornavnet ditt
-            </div>
-          </label>
+    <>
+      <form onSubmit={submitForm}>
+        <h1 className={styles.title}>
+          Fyll inn og dette skjemaet for å bestille vaksine
+        </h1>
+        <div className={styles.container}>
+          <div className={styles.formGrouping}>
+            <label className={styles.inputLabel}>
+              <span>Hva heter du?</span>
+              <input
+                required
+                name="first"
+                aria-describedby="nameDescription"
+                aria-errormessage="Fyll inn fornavnet ditt"
+              />
+              <div id="nameDescription" className={styles.visuallyHidden}>
+                Fyll inn fornavnet ditt
+              </div>
+            </label>
 
-          <label className={styles.inputLabel}>
-            <span>Hvilken by bor du i?</span>
-            <input name="city" />
-          </label>
+            <label className={styles.inputLabel}>
+              <span>Hvilken by bor du i?</span>
+              <input name="city" />
+            </label>
 
-          <label className={styles.inputLabel}>
-            <span>Hvor gammel er du?</span>
-            <input
-              type="number"
-              required
-              name="birthyear"
-              min="0"
-              max="150"
-              aria-describedby="ageDescription"
-            />
-          </label>
+            <label className={styles.inputLabel}>
+              <span>Hvor gammel er du?</span>
+              <input
+                type="number"
+                required
+                name="birthyear"
+                min="0"
+                max="150"
+                aria-describedby="ageDescription"
+              />
+            </label>
+          </div>
+
+          <fieldset role="radiogroup" className={styles.agreeToTerms}>
+            <legend>Godta vilkårene for å gå videre</legend>
+            <label>
+              <span>Ja, jeg godtar</span>
+              <input type="radio" required name="terms" value="yes" />
+            </label>
+            <label>
+              <span>Nei</span>
+              <input type="radio" required name="terms" value="no" />
+            </label>
+          </fieldset>
+
+          <div className={styles.buttons}>
+            <button type="reset" className={styles.resetButton}>
+              Avbryt
+            </button>
+            <button>Bestill vaksine</button>
+          </div>
         </div>
+      </form>
 
-        <fieldset role="radiogroup">
-          <legend>Bekreft vilkårene for å gå videre</legend>
-          <label>
-            <span>Ja</span>
-            <input type="radio" required name="terms" value="yes" />
-          </label>
-          <label>
-            <span>Nei</span>
-            <input type="radio" required name="terms" value="no" />
-          </label>
-        </fieldset>
+      <form
+        className={[styles.container, styles.successContainer].join(" ")}
+        onSubmit={advance}
+        aria-live="assertive"
+        aria-hidden={!showResults}
+        hidden={!showResults}
+      >
+        <p>Gratulerer! Du kan nå gå videre for velge en time</p>
         <button>Gå videre</button>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
