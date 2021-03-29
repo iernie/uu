dde99d5de9fb54584eba0f1628bf5cf2129e9ae1
Eirik Luka <eirik.luka@gmail.com>
2021-03-29 20:13:15 +0200

    Legg til skjermleser-tips

--- src/ScreenReaderTips/ScreenReaderTips.jsx
+++ src/ScreenReaderTips/ScreenReaderTips.jsx
@@ -5,17 +5,22 @@ const ScreenReaderTips = () => {
     <section aria-hidden className={styles.screenReaderTips}>
       Noen enkle skjermlesertips
       <h2>VoiceOver</h2>
-      <ul>
+      <p>
+        <code>VO</code> under tilsvarer «VoiceOver modifier», og er som standard{" "}
+        <code>CTRL + Option</code> eller <code>CAPS LOCK</code>
+      </p>
+      <ul className={styles.list}>
         <li>
-          Trippelklikk på fingeravtrykkleseren for å aktivere/deaktivere
-          VoiceOver
+          Hold inne <code>CMD</code> og trippelklikk på fingeravtrykkleseren for
+          å aktivere/deaktivere VoiceOver
         </li>
         <li>
-          Åpne Rotor: <code>VO+u</code>. Man kan bla gjennom forskjellige lister
-          med piltastene
+          Åpne Rotor: <code>VO + u</code>. Man kan bla gjennom forskjellige
+          lister med piltastene
         </li>
         <li>
-          Åpne Rotor: <code>VO+u</code>
+          Bla til forrige/neste element på siden:{" "}
+          <code>VO + pil venstre / VO + pil høyre</code>
         </li>
       </ul>
     </section>
