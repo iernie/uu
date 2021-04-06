9007d8601f82ad318d6c143a42b63e09b1a3b32f
einar afiouni <einar.afiouni@bekk.no>
2021-04-06 18:27:31 +0200

    build

---
+++ build/static/js/react-syntax-highlighter_languages_refractor_ini.20568219.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpuu=this.webpackJsonpuu||[]).push([[60],{175:function(t,n,s){"use strict";function i(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=i,i.displayName="ini",i.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.20568219.chunk.js.map
