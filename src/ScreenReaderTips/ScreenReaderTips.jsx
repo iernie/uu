2112e1e33ffd750913260af4cc241093eb5a4a14
Eirik Luka <eirik.luka@gmail.com>
2021-04-06 20:04:49 +0200

    Gjør det mulig å skjule tipsene

--- src/ScreenReaderTips/ScreenReaderTips.jsx
+++ src/ScreenReaderTips/ScreenReaderTips.jsx
@@ -1,29 +1,39 @@
+import { useState } from "react";
 import styles from "./ScreenReaderTips.module.scss";
 
 const ScreenReaderTips = () => {
+  const [showTips, setShowTips] = useState(true);
   return (
     <div aria-hidden="true" className={styles.screenReaderTips}>
-      Noen enkle skjermlesertips
-      <h2>VoiceOver</h2>
-      <p>
-        <code>VO</code> under tilsvarer «VoiceOver modifier», og er som standard{" "}
-        <code>CTRL + Option</code> eller <code>CAPS LOCK</code>
-      </p>
-      <ul className={styles.list}>
-        <li>
-          Hold inne <code>CMD</code> og trippelklikk på fingeravtrykkleseren for
-          å aktivere/deaktivere VoiceOver
-        </li>
-        <li>
-          Åpne Rotor: <code>VO + u</code>. Man kan bla gjennom forskjellige
-          lister med piltastene. (Headings, Form controls, Web spots, Landmarks,
-          Articles, Window spots)
-        </li>
-        <li>
-          Bla til forrige/neste element på siden:{" "}
-          <code>VO + pil venstre / VO + pil høyre</code>
-        </li>
-      </ul>
+      <div hidden={!showTips}>
+        <h2>Noen enkle skjermlesertips i VoiceOver</h2>
+        <p>
+          <code>VO</code> under tilsvarer «VoiceOver modifier», og er som
+          standard <code>CTRL + Option</code> eller <code>CAPS LOCK</code>
+        </p>
+        <ul className={styles.list}>
+          <li>
+            Hold inne <code>CMD</code> og trippelklikk på fingeravtrykkleseren
+            for å aktivere/deaktivere VoiceOver
+          </li>
+          <li>
+            Åpne Rotor: <code>VO + u</code>. Man kan bla gjennom forskjellige
+            lister med piltastene. (Headings, Form controls, Web spots,
+            Landmarks, Articles, Window spots)
+          </li>
+          <li>
+            Bla til forrige/neste element på siden:{" "}
+            <code>VO + pil venstre / VO + pil høyre</code>
+          </li>
+        </ul>
+      </div>
+
+      <button
+        onClick={() => setShowTips(!showTips)}
+        className={styles.toggleTips}
+      >
+        {showTips ? "Skjul tips" : "Vis VoiceOver tips"}
+      </button>
     </div>
   );
 };
