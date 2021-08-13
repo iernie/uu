2ab10f486f5690c2bec81564350cf5ffaddc5607
einar afiouni <einar.afiouni@bekk.no>
2021-08-13 13:25:10 +0200

    endrer på første oppgave

--- src/Task0/Task0.jsx
+++ src/Task0/Task0.jsx
@@ -2,39 +2,63 @@ import { useState } from "react";
 import Slide from "../Slide/Slide";
 import styles from "./Task0.module.scss";
 
+export const onKeyDown = (callback) => (event) => {
+  return (event) => {
+    // SPACE OR ENTER
+    if (event.keyCode === 13 || event.keyCode === 32) {
+      if (callback) callback(event);
+    }
+  };
+};
+
 const Task0 = ({ onSubmit }) => {
-  const [value, setValue] = useState("");
   const [error, setError] = useState(false);
+
+  const onError = () => setError(true);
+
   return (
     <Slide>
       <div className={styles.main}>
         <p className={styles.introText}>
           En venn ringte deg og foralte deg at nå kan alle få vaksine. Du lurer
-          på om dette er sant så du går inn på FHI sine sider for å finne mer
-          informasjon. Du er svaksynt og trenger en skjermleser for å finne ut
-          hva du vil gjøre ...
+          på om dette er sant så du går inn på FHI sine sider for å finne ut
+          mer. Du er svaksynt og trenger en skjermleser for å finne ut hva du må
+          trykke på for å få informasjon om vaksinering.
         </p>
         <article className="blurredContent-1">
           <h1>FHI</h1>
           <h2>AKTUELT</h2>
           <div className={styles.container}>
-            <a href="/#0" className={styles.item}>
+            <a
+              href="#0"
+              className={styles.item}
+              onClick={onError}
+              onKeyDown={onKeyDown(onError)}
+            >
+              <img
+                alt="Koronavaksinasjons-programmet"
+                src="https://www.fhi.no/globalassets/bilder/alle-tematopper/film1_beskyttelse-2.png?preset=onethirdwidth"
+              />
+              <h3>Koronavaksinasjons-programmet</h3>
+            </a>
+            <a
+              href="#0"
+              className={styles.item}
+              onClick={onError}
+              onKeyDown={onKeyDown(onError)}
+            >
               <img
                 alt="Koronavirus"
                 src="https://www.fhi.no/contentassets/5aa334ec568e4b069a701b7e7a4b7686/koronavirus-tema21.png?preset=onethirdwidth"
               />
               <h3>Koronavirus</h3>
             </a>
-            <a href="/#0" className={styles.item}>
-              <img
-                alt="Koronavaksinasjons-programmet"
-                src="https://www.fhi.no/globalassets/bilder/alle-tematopper/film1_beskyttelse-2.png?preset=onethirdwidth"
-              />
-              <h3 aria-label="Jeg vil vaksinere">
-                Koronavaksinasjons-programmet
-              </h3>
-            </a>
-            <a href="/#0" className={styles.item}>
+            <a
+              href="#0"
+              className={styles.item}
+              onClick={onError}
+              onKeyDown={onKeyDown(onError)}
+            >
               <img
                 alt="Smittestopp"
                 src="https://www.fhi.no/globalassets/bilder/smittestopp/smittestopp-logo-temaside.png?preset=onethirdwidth"
@@ -44,17 +68,37 @@ const Task0 = ({ onSubmit }) => {
           </div>
           <h2>TJENESTER OG RESSURSER</h2>
           <div className={styles.container2}>
-            <a className={styles.item2} href="/#0">
+            <a
+              className={styles.item2}
+              href="#0"
+              onClick={onError}
+              onKeyDown={onKeyDown(onError)}
+            >
               Beredskaps- og vakttelefoner
             </a>
-            <a className={styles.item2} href="/#0">
+            <a
+              className={styles.item2}
+              href="#0"
+              onClick={onError}
+              onKeyDown={onKeyDown(onError)}
+            >
               Melding og varsling om smittsom sykdom
             </a>
-            <a className={styles.item2} href="/#0">
+            <a
+              className={styles.item2}
+              href="#0"
+              onClick={onError}
+              onKeyDown={onKeyDown(onError)}
+            >
               Laboratorieanalyser
             </a>
-            <a className={styles.item2} href="/#0">
-              Statistikkbankene i FHI
+            <a
+              className={styles.item2}
+              href="#0"
+              onClick={onSubmit}
+              onKeyDown={onKeyDown(onSubmit)}
+            >
+              Informasjon om vaksinering
             </a>
           </div>
         </article>
@@ -63,26 +107,6 @@ const Task0 = ({ onSubmit }) => {
             Nei, det er ikke det du vil. Prøv igjen.
           </div>
         )}
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            setError(false);
-            if (value.toLowerCase() === "vaksinere") {
-              onSubmit();
-            } else {
-              setError(true);
-            }
-          }}
-        >
-          <input
-            value={value}
-            className={styles.input}
-            style={{ border: error ? "1px solid red" : "" }}
-            placeholder="Hva er det du vil gjøre?"
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <button className={styles.button}>Gå videre</button>
-        </form>
       </div>
     </Slide>
   );
